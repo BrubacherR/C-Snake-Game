eda06012dd39986dbaf98eb41f80c8d59bb0c5c4
Ryan Brubacher <144581798+BrubacherR@users.noreply.github.com>
2024-09-13 11:09:47 -0400

    Created food generation class and functions

---
+++ Food.h
@@ -0,0 +1,32 @@
+#ifndef FOOD_H
+#define FOOD_H
+
+#include<stdlib.h>
+#include <time.h> 
+
+#include "objPos.h"
+#include "objPosArrayList.h"
+#include "GameMechs.h"
+#include "objPos.h"
+#include "objPosArrayList.h"
+
+
+using namespace std; 
+
+class Food{
+
+    private: 
+        objPos foodPos; 
+        GameMechs* mainGameMechsRef;
+
+    public: 
+        Food(GameMechs* thisGMRef);
+        ~Food();
+
+        void generateFood (objPosArrayList *blockoff); 
+        void getFoodPos ( objPos &objPos); 
+
+
+};
+
+#endif
